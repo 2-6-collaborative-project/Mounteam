8c741391a13ab4c47f746398660c5f6e0591ed10
9ummy9 <ria1005@kakao.com>
2024-03-18 17:27:35 +0900

    Design: prefer font-size 수정

--- app/preference/page.tsx
+++ app/preference/page.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import { Radio, Checkbox } from 'antd';
 import type { RadioChangeEvent } from 'antd';
 import type { CheckboxChangeEvent } from 'antd/lib/checkbox';
+import { ConfigProvider } from 'antd';
 
 interface ButtonProps {
   $isActive: boolean;
@@ -49,6 +50,7 @@ const Notification = styled.div`
   font-size: 1rem;
   font-style: normal;
   font-weight: 700;
+  line-height: 2rem; /* 200% */
   @media (max-width: 480px) {
     p {
       display: inline;
@@ -94,6 +96,12 @@ const CheckboxCol = styled.div`
   flex-direction: column;
   gap: 0.625rem;
   padding: 4.06rem 0 5.81rem;
+  /* Heading/16 */
+  /* font-family: Pretendard; */
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.5rem; /* 150% */
 `;
 
 const ButtonContainer = styled.div`
@@ -161,82 +169,89 @@ export default function Preference() {
     gender && age && region && locationConsent && personalInfoConsent;
 
   return (
-    <Body>
-      <Logo>
-        <Image
-          src="/logo.svg"
-          alt="logo icon"
-          width={277.24}
-          height={93.19}
-          priority
-        />
-      </Logo>
-      <Notification>
-        <p>
-          성별, 연령대, 관심지역 정보를 통해 맞춤형 등산 모임과 코스를
-          추천해드립니다.
-        </p>
-        <p>
-          이 정보는 더 적합하고 만족스러운 등산 경험을 제공하기 위해서만
-          사용됩니다.
-        </p>
-      </Notification>
+    <ConfigProvider
+      theme={{
+        token: {
+          fontSize: 10,
+        },
+      }}
+    >
+      <Body>
+        <Logo>
+          <Image
+            src="/logo.svg"
+            alt="logo icon"
+            width={277.24}
+            height={93.19}
+            priority
+          />
+        </Logo>
+        <Notification>
+          <p>
+            성별, 연령대, 관심지역 정보를 통해 맞춤형 등산 모임과 코스를
+            추천해드립니다.
+          </p>
+          <p>
+            이 정보는 더 적합하고 만족스러운 등산 경험을 제공하기 위해서만
+            사용됩니다.
+          </p>
+        </Notification>
 
-      <RadioCol>
-        <RadioLabel>성별</RadioLabel>
-        <Radio.Group onChange={onGenderChange} value={gender}>
-          <Radio value={1}>여성</Radio>
-          <Radio value={2}>남성</Radio>
-        </Radio.Group>
+        <RadioCol>
+          <RadioLabel>성별</RadioLabel>
+          <Radio.Group onChange={onGenderChange} value={gender}>
+            <Radio value={1}>여성</Radio>
+            <Radio value={2}>남성</Radio>
+          </Radio.Group>
+          <RadioLabel>연령대</RadioLabel>
+          <Radio.Group onChange={onAgeChange} value={age}>
+            <Radio value={1}>10대</Radio>
+            <Radio value={2}>20대</Radio>
+            <Radio value={3}>30대</Radio>
+            <Radio value={4}>40대</Radio>
+            <Radio value={5}>50대</Radio>
+            <Radio value={6}>60대 이상</Radio>
+          </Radio.Group>
 
-        <RadioLabel>연령대</RadioLabel>
-        <Radio.Group onChange={onAgeChange} value={age}>
-          <Radio value={1}>10대</Radio>
-          <Radio value={2}>20대</Radio>
-          <Radio value={3}>30대</Radio>
-          <Radio value={4}>40대</Radio>
-          <Radio value={5}>50대</Radio>
-          <Radio value={6}>60대 이상</Radio>
-        </Radio.Group>
+          <RadioLabel>관심지역</RadioLabel>
+          <RegionCol>
+            <div>
+              <Radio.Group onChange={onRegionChange} value={region}>
+                <Radio value={1}>서울</Radio>
+                <Radio value={2}>경기도</Radio>
+                <Radio value={3}>강원도</Radio>
+                <Radio value={4}>충청북도</Radio>
+                <Radio value={5}>충청남도</Radio>
+              </Radio.Group>
+            </div>
+            <div>
+              <Radio.Group onChange={onRegionChange} value={region}>
+                <Radio value={6}>전라북도</Radio>
+                <Radio value={7}>전라남도</Radio>
+                <Radio value={8}>경상북도</Radio>
+                <Radio value={9}>경상남도</Radio>
+                <Radio value={10}>제주도</Radio>
+              </Radio.Group>
+            </div>
+          </RegionCol>
+        </RadioCol>
 
-        <RadioLabel>관심지역</RadioLabel>
-        <RegionCol>
+        <CheckboxCol>
           <div>
-            <Radio.Group onChange={onRegionChange} value={region}>
-              <Radio value={1}>서울</Radio>
-              <Radio value={2}>경기도</Radio>
-              <Radio value={3}>강원도</Radio>
-              <Radio value={4}>충청북도</Radio>
-              <Radio value={5}>충청남도</Radio>
-            </Radio.Group>
+            <Checkbox onChange={onLocationConsentChange}>
+              위치 기능 동의 여부
+            </Checkbox>
           </div>
           <div>
-            <Radio.Group onChange={onRegionChange} value={region}>
-              <Radio value={6}>전라북도</Radio>
-              <Radio value={7}>전라남도</Radio>
-              <Radio value={8}>경상북도</Radio>
-              <Radio value={9}>경상남도</Radio>
-              <Radio value={10}>제주도</Radio>
-            </Radio.Group>
+            <Checkbox onChange={onPersonalInfoConsentChange}>
+              개인 정보 사용 동의 여부
+            </Checkbox>
           </div>
-        </RegionCol>
-      </RadioCol>
-
-      <CheckboxCol>
-        <div>
-          <Checkbox onChange={onLocationConsentChange}>
-            위치 기능 동의 여부
-          </Checkbox>
-        </div>
-        <div>
-          <Checkbox onChange={onPersonalInfoConsentChange}>
-            개인 정보 사용 동의 여부
-          </Checkbox>
-        </div>
-      </CheckboxCol>
-      <ButtonContainer>
-        <Button $isActive={isButtonActive}>시작하기</Button>
-      </ButtonContainer>
-    </Body>
+        </CheckboxCol>
+        <ButtonContainer>
+          <Button $isActive={isButtonActive}>시작하기</Button>
+        </ButtonContainer>
+      </Body>
+    </ConfigProvider>
   );
 }
