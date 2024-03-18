c7469d911e7c9b7ec6d7d40187c596bbd9bde221
DongKyuK12 <jkdk981024@gmail.com>
2024-03-18 19:27:44 +0900

    feat : profile 컴포넌트 완성

--- src/components/mypage/Profile.tsx
+++ src/components/mypage/Profile.tsx
@@ -1,105 +1,178 @@
 import Image from 'next/image';
 import styled from 'styled-components';
+import Avatars from '../shared/Avatar';
 
 const ProfileContainer = styled.div`
-  width: 99.1rem;
-  display: flex;
-  gap: 5.7rem;
-`;
-
-const ProfileInformation = styled.div`
-  width: 74.2rem;
-
+  max-width: 98.8rem;
+  height: 12.8rem;
   display: flex;
   justify-content: space-between;
-  align-items: flex-end;
-`;
-
-const ImageAndUsername = styled.div`
-  display: flex;
   align-items: end;
-  gap: 1.7rem;
 `;
 
-const ProfileImg = styled.div`
-  width: 12.8rem;
+const AvatarContainer = styled.div`
   height: 12.8rem;
-  position: relative;
-  border-radius: 50%;
-  overflow: hidden;
+  display: flex;
+  gap: 2rem;
 `;
 
-const TagAndUsername = styled.div`
+const TagsAndName = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 1rem;
+`;
+
+const Preferences = styled.div`
+  height: 2.6rem;
+  display: flex;
   gap: 0.7rem;
 `;
 
-const LevelAndUsername = styled.div`
+const Preference = styled.div`
+  height: 2.6rem;
+  padding: 0 0.7rem 0 0.7rem;
+
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 3px;
+  background-color: #d3dcfd;
+  text-align: center;
+  color: #0331d1;
+  font-size: 1.4rem;
+  font-weight: 600;
+  line-height: 2rem;
+`;
+
+const LevelAndNickname = styled.div`
   display: flex;
+  flex-direction: column;
 `;
 
-const UserLevel = styled.div`
-  color: black;
-  font-size: 2rem;
+const Level = styled.p`
+  color: var(--black-000000);
+  font-size: 1.6rem;
+  font-weight: 700;
+  line-height: 3.2rem;
+`;
+
+const NicknameContainer = styled.div`
+  display: flex;
+  gap: 0.7rem;
+  align-items: center;
+`;
+
+const Nickname = styled.p`
+  color: var(--black-000000);
+  font-size: 2.4rem;
+  font-weight: 600;
+  line-height: 3.2rem;
+`;
+
+const Description = styled.p`
+  color: var(--black-000000);
+  font-size: 1.6rem;
+  font-weight: 700;
+  line-height: 3.2rem;
+`;
+
+const BadgeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+`;
+
+const BadgeTitleContainer = styled.div`
+  max-width: 99rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const BadgeTitle = styled.div`
+  color: var(--black-000000);
+  font-size: 2.4rem;
   font-weight: 700;
   line-height: 2.4rem;
 `;
 
-const Username = styled.div`
-  color: black;
-  font-size: 2rem;
-  font-weight: 700;
-  line-height: 2.4rem;
+const OptionContainer = styled.div`
+  display: flex;
+  gap: 1.8rem;
 `;
 
-const Tags = styled.div``;
-
-const Description = styled.div`
-  width: 40rem;
-  height: auto;
-
-  color: black;
-  font-size: 2rem;
-  font-family: Gapyeong Wave;
-  font-weight: 400;
+const Option = styled.p`
+  color: var(--black-000000);
+  font-size: 1.4rem;
+  font-weight: 600;
+  line-height: 2rem;
+`;
+const Badges = styled.div`
+  display: flex;
+  gap: 0.3rem;
 `;
 
-const Batges = styled.div``;
+const Badge = styled.div``;
 
-const Records = styled.div``;
+interface ProfileProps {
+  level: number;
+  name: string;
+  img: string;
+  preference: string[] | undefined;
+  description: string;
+  clickShowAll: () => void;
+  clickVerify: () => void;
+}
 
-export default function Profile() {
+export default function Profile({
+  level,
+  name,
+  img,
+  preference,
+  description,
+  clickShowAll,
+  clickVerify,
+}: ProfileProps) {
   return (
     <>
       <ProfileContainer>
-        <ProfileInformation>
-          <ImageAndUsername>
-            <ProfileImg>
-              <Image
-                src="/user.svg"
-                alt="프로필이미지"
-                layout="fill"
-                loading="eager"
-              />
-            </ProfileImg>
-            <TagAndUsername>
-              <Tags>태그1</Tags>
-              <LevelAndUsername>
-                <UserLevel>{'Lv1.'}</UserLevel> <Username>{'닉네임'}</Username>
-              </LevelAndUsername>
-            </TagAndUsername>
-          </ImageAndUsername>
-          <Description>
-            {
-              '온갖 일들이 규칙적으로 묶여 있는 오늘날, 우리 생활 속에 남이 있는 비록 일시적이나마 완전한 자유로운 삶의 방식 하나가 등산이다.'
-            }
-          </Description>
-        </ProfileInformation>
-        <Image src="/user.svg" alt="수정" width={24} height={24} />
+        <AvatarContainer>
+          <Avatars type="profile" img={img} />
+          <TagsAndName>
+            <Preferences>
+              {preference?.map((item: string, index: number) => (
+                <Preference key={'preference' + index}>{item}</Preference>
+              ))}
+            </Preferences>
+            <LevelAndNickname>
+              <Level>{'Lv.' + level}</Level>
+              <NicknameContainer>
+                <Nickname>{name + 'd'}</Nickname>
+                <Image
+                  alt="프로필수정"
+                  src="/edit.svg"
+                  width={16}
+                  height={16}
+                />
+              </NicknameContainer>
+            </LevelAndNickname>
+          </TagsAndName>
+        </AvatarContainer>
+        <Description>{description}</Description>
       </ProfileContainer>
-      <Batges></Batges>
-      <Records></Records>
+      <BadgeContainer>
+        <BadgeTitleContainer>
+          <BadgeTitle>내 뱃지</BadgeTitle>
+          <OptionContainer>
+            <Option onClick={clickShowAll}>전체보기</Option>
+            <Option onClick={clickVerify}>인증하기</Option>
+          </OptionContainer>
+        </BadgeTitleContainer>
+        <Badges>
+          <Badge></Badge>
+        </Badges>
+      </BadgeContainer>
     </>
   );
 }
