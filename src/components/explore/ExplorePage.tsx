d28d9b1343841ef14c9bd585a1e41cbb4c4c0d50
handsup <dhfma3394@naver.com>
2024-03-24 01:38:36 +0900

    Feat: 검색 결과 파라미터 반영되도록 구현, 검색별 상황에 대한 결과 대응코드 구현

--- src/components/explore/ExplorePage.tsx
+++ src/components/explore/ExplorePage.tsx
@@ -1,5 +1,6 @@
 'use client';
-import { useState } from 'react';
+
+import { useCallback, useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import styled from 'styled-components';
 import KakaoMap from '@/src/components/explore/KakaoMap';
@@ -7,6 +8,8 @@ import MountainInfo from '@/src/components/shared/MountainInfo';
 import ExploreFilterPanel from '@/src/components/explore/ExploreFilterPanel';
 import getMountainData from '@/src/components/explore/getMountainData';
 import { AutoComplete, Input } from 'antd';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+
 const SearchMountainArea = styled.div`
   margin-top: 7rem;
 `;
@@ -14,20 +17,6 @@ const MainTitle = styled.h2`
   font-size: 3rem;
   font-weight: 600;
   line-height: 4.2rem;
-`;
-
-const SearchTagContainer = styled.div`
-  display: inline-flex;
-  align-items: flex-start;
-  gap: 0.8rem;
-  margin: 2rem 0 4rem 0;
-`;
-
-const SearchTag = styled.span`
-  padding: 0.1rem 0.8rem;
-  border-radius: 0.2rem;
-  border: 1px solid var(--Neutral-5, #d9d9d9);
-  background: var(--Neutral-2, #fafafa);
 `;
 
 const SearchResultArea = styled.div`
@@ -106,57 +95,29 @@ const SearchContainer = styled.div`
   }
 `;
 
-const Search = styled.input`
-  border: 0;
-  padding-left: 10px;
-  background-color: #eaeaea;
-  width: 100%;
-  height: 100%;
-  outline: none;
-`;
-
-const AutoSearchContainer = styled.div`
-  z-index: 3;
-  height: 50vh;
-  width: 400px;
-  background-color: #fff;
-  position: absolute;
-  top: 45px;
-  border: 2px solid;
-  padding: 15px;
-`;
-
-const AutoSearchWrap = styled.ul``;
-
-const AutoSearchData = styled.li`
-  padding: 10px 8px;
-  width: 100%;
-  font-size: 14px;
-  font-weight: bold;
-  z-index: 4;
-  letter-spacing: 2px;
-  &:hover {
-    background-color: #edf5f5;
-    cursor: pointer;
-  }
-  position: relative;
-  img {
-    position: absolute;
-    right: 5px;
-    width: 18px;
-    top: 50%;
-    transform: translateY(-50%);
-  }
-`;
-
 export default function ExplorePage() {
-  const [keyword, setKeyword] = useState<string>('');
-  const [selectedMountain, setSelectedMountain] = useState(null);
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const initKeyword = searchParams.get('mountain');
+
+  const [keyword, setKeyword] = useState<string>(initKeyword || '');
+  const [searchedMountain, setSearchedMountain] = useState(null);
 
   const { data: mountainList } = useQuery({
     queryKey: ['mountainList'],
     queryFn: () => getMountainData(),
   });
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams],
+  );
 
   const handleInputChange = (value: string) => {
     setKeyword(value);
@@ -166,16 +127,25 @@ export default function ExplorePage() {
     value: list.명산_이름,
   }));
 
-  const handleSelectMountain = (value: string) => {
-    const selected = mountainList.find((list: any) => list.명산_이름 === value);
-
-    setSelectedMountain(selected);
+  const handleSearch = (value: string) => {
+    // URL 쿼리스트링 업데이트
+    router.push(pathname + '?' + createQueryString('mountain', value));
     setKeyword(value);
   };
 
   const filteredOptions = options?.filter((option: any) =>
     option.value.includes(keyword),
   );
+
+  useEffect(() => {
+    if (initKeyword) {
+      const searched = mountainList?.find(
+        (list: any) => list.명산_이름 === initKeyword,
+      );
+
+      setSearchedMountain(searched);
+    }
+  }, [initKeyword, mountainList]);
 
   return (
     <>
@@ -194,22 +164,24 @@ export default function ExplorePage() {
           <SearchContainer>
             <AutoComplete
               options={filteredOptions}
-              onSelect={handleSelectMountain}
+              onSelect={handleSearch}
               onSearch={handleInputChange}
               value={keyword}
             >
               <Input.Search
                 placeholder="대한민국 산 탐험하기"
+                onSearch={handleSearch}
                 enterButton
-                onSearch={handleSelectMountain}
               />
             </AutoComplete>
           </SearchContainer>
 
-          <KakaoMap
-            mountainList={mountainList}
-            selectedMountain={selectedMountain}
-          />
+          {
+            <KakaoMap
+              mountainList={mountainList}
+              selectedMountain={searchedMountain}
+            />
+          }
         </SearchMountainArea>
 
         <SearchResultArea>
@@ -223,12 +195,25 @@ export default function ExplorePage() {
             </MountainListHeader>
 
             <MountainList>
-              {selectedMountain ? (
-                <MountainInfo list={selectedMountain} />
-              ) : (
+              {initKeyword === '' &&
                 mountainList?.map((list: any) => (
                   <MountainInfo key={list.X좌표} list={list} />
-                ))
+                ))}
+
+              {/* /explore 처음 진입했을떄 */}
+              {!initKeyword &&
+                !searchedMountain &&
+                mountainList?.map((list: any) => (
+                  <MountainInfo key={list.X좌표} list={list} />
+                ))}
+              {/* 검색한 산이 있을때 */}
+              {initKeyword && searchedMountain && (
+                <MountainInfo list={searchedMountain} />
+              )}
+
+              {/* 검색한 산이 없을때 */}
+              {initKeyword && !searchedMountain && (
+                <div>검색 결과가 없습니다.</div>
               )}
             </MountainList>
           </MountainListContainer>
