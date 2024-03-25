ab28317b3a47e5b2779124981b66136adfd26168
9ummy9 <ria1005@kakao.com>
2024-03-25 23:21:08 +0900

    Chore: tab 컴포넌트 수정

--- src/components/shared/Tab.tsx
+++ src/components/shared/Tab.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import typography from '@/app/styles/typography';
 import { colors } from '@/app/styles/colors';
 
@@ -19,10 +19,9 @@ const LinkCol = styled.div`
 const StyledParagraph = styled.p<{ $variant?: Variant }>`
   padding: 1rem 0rem;
 
-  color: ${({ $variant }) =>
-    $variant === 'main' ? colors.Primary[500] : 'inherit'};
+  color: ${({ $variant }) => ($variant ? colors.Primary[500] : 'inherit')};
   border-bottom: ${({ $variant }) =>
-    $variant === 'main' ? `1px solid ${colors.Primary[500]}` : 'none'};
+    $variant ? `1px solid ${colors.Primary[500]}` : 'none'};
 
   &:hover {
     color: ${colors.Primary[500]};
@@ -39,7 +38,9 @@ export default function Tab({ variant }: TabProps) {
     <div>
       <LinkCol>
         <Link href="/" passHref>
-          <StyledParagraph $variant={variant}>추천</StyledParagraph>
+          <StyledParagraph $variant={variant === 'main' ? 'main' : undefined}>
+            추천
+          </StyledParagraph>
         </Link>
         <Link href="/explores" passHref>
           <StyledParagraph
