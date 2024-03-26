d63aebc7d21054040eee3eee924cfbda3225ea30
9ummy9 <ria1005@kakao.com>
2024-03-26 21:46:27 +0900

    Chore: user icon 원상복구

--- app/(withHeader)/teams/page.tsx
+++ app/(withHeader)/teams/page.tsx
@@ -3,6 +3,7 @@
 import styled from 'styled-components';
 import typography from '@/app/styles/typography';
 import Tab from '@/src/components/shared/Tab';
+import TeamFilterPanel from '@/src/components/teams/TeamFilterPanel';
 
 const Title = styled.div`
   ${typography.Heading30};
@@ -20,6 +21,7 @@ export default function TeamPage() {
       <Tab variant="teams" />
       <Title>전체 등산 모임</Title>
       <div>검색 바</div>
+      <TeamFilterPanel />
     </>
   );
 }
