b1ed333d36adaf2a92f7a20bea8d3ab67afe06c3
9ummy9 <ria1005@kakao.com>
2024-03-26 23:38:08 +0900

    Design: 모임페이지 UI 구현

--- app/(withHeader)/teams/page.tsx
+++ app/(withHeader)/teams/page.tsx
@@ -1,27 +1,117 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import typography from '@/app/styles/typography';
 import Tab from '@/src/components/shared/Tab';
+import typography from '@/app/styles/typography';
 import TeamFilterPanel from '@/src/components/teams/TeamFilterPanel';
+import { colors } from '@/app/styles/colors';
+import TeamThumbnail from '@/src/components/shared/TeamThumbnail';
+import { teamFeed } from '@/src/lib/mockData';
 
-const Title = styled.div`
+const SearchTeamArea = styled.div``;
+
+const MainTitle = styled.h2`
+  margin-top: 2.5rem;
+  margin-bottom: 5.8rem;
   ${typography.Heading30};
-  padding: 1.88rem 0 5rem 0;
+`;
 
+const SearchResultArea = styled.div`
+  display: flex;
+  gap: 9rem;
+  margin-top: 6rem;
+`;
+
+const FilterContainer = styled.div`
+  width: 20rem;
+`;
+
+const TeamListContainer = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  gap: 1.5625rem;
+  width: 100%;
+`;
+
+const TeamListHeader = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  color: ${colors.Grayscale[7]};
+  ${typography.Footnote14};
+  gap: 0.5rem;
+  margin-left: auto; // TeamList 정렬 변경에 따라 오른쪽 정렬 방식 변경
+  // 정렬 선택 시 색상 ${colors.Grayscale[13]}으로 변경 필요
+`;
+
+// 모임 리스트 렌더링하는 부분이라 탐험페이지와 다릅니다
+const TeamList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 2.1875rem;
+`;
+
+const Container = styled.div`
   @media (max-width: 768px) {
-    ${typography.Heading24};
-    padding: 1.25rem 0 5rem 0;
+    ${MainTitle} {
+      font-size: 2rem;
+      line-height: 2.5rem;
+    }
+
+    ${SearchResultArea} {
+      flex-direction: column;
+    }
+
+    ${FilterContainer} {
+      width: 100%;
+    }
   }
 `;
 
-export default function TeamPage() {
+export default function TeamsPage() {
+  const [teams, setTeams] = useState<
+    {
+      teamId: number;
+      exploreId: string;
+      title: string;
+      departureDay: string;
+      ageRange: string[];
+      genderRange: string;
+    }[]
+  >([]);
+
+  useEffect(() => {
+    setTeams(teamFeed);
+  }, []);
+
   return (
     <>
-      <Tab variant="teams" />
-      <Title>전체 등산 모임</Title>
-      <div>검색 바</div>
-      <TeamFilterPanel />
+      <Container>
+        <Tab variant="teams" />
+        <SearchTeamArea>
+          <MainTitle>전체 등산 모임</MainTitle>
+        </SearchTeamArea>
+
+        <SearchResultArea>
+          <FilterContainer>
+            <TeamFilterPanel />
+          </FilterContainer>
+
+          <TeamListContainer>
+            <TeamListHeader>
+              <p>가나다순</p>
+              <p> | </p>
+              <p>인기순</p>
+            </TeamListHeader>
+            <TeamList>
+              {teams.map((team) => (
+                <TeamThumbnail key={team.teamId} team={team} />
+              ))}
+            </TeamList>
+          </TeamListContainer>
+        </SearchResultArea>
+      </Container>
     </>
   );
 }
