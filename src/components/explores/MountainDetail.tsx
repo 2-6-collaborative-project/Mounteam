cf36daa570cddd680d3efb8ff1bd22a6f7817274
handsup <dhfma3394@naver.com>
2024-03-29 23:17:44 +0900

    Design: 산 상세 페이지 레이아웃 완성

--- src/components/explores/MountainDetail.tsx
+++ src/components/explores/MountainDetail.tsx
@@ -1,70 +1,121 @@
 'use client';
 
+import Image from 'next/image';
 import styled from 'styled-components';
 import KakaoMap from './ExplorePage/KakaoMap';
 import mountainDataProps from '@/src/types/mountainDataProps';
+import Tab from '@/src/components/shared/Tab';
+import Link from 'next/link';
 
-const Container = styled.div`
-  margin: 3.2rem 10.4rem;
-`;
-
-const MainTitle = styled.h2`
-  font-size: 3rem;
-  font-weight: 600;
-  line-height: 4.2rem;
+const MapContainer = styled.div`
+  margin-top: 9rem;
 `;
 
 const ActivityTabs = styled.div`
   display: flex;
-  justify-content: space-evenly;
-  margin-top: 2rem;
-`;
-const ActivityTab = styled.p`
-  font-size: 2rem;
-  font-weight: 500;
+  padding: 2.5rem 0rem;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid #000;
 `;
 
-const CourseInfoArea = styled.div`
-  margin-top: 2rem;
-`;
-const CourseTotalCount = styled(MainTitle)`
-  margin-bottom: 1.6rem;
-  color: var(--xn-hu-5-b-2-ji-36-aba-com-ship-gray, #3f3f46);
-  font-size: 2rem;
-  line-height: 2.5rem;
+const StyledLink = styled(Link)`
+  width: 100%;
 `;
 
-const CourseGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(0, 26.25rem));
-  column-gap: 2rem;
-  row-gap: 2rem;
-`;
-
-const CourseInfo = styled.div`
+const ActivityTab = styled.div<{ $team?: boolean }>`
   display: flex;
-  gap: 0.5rem;
-  padding: 1rem 1.5rem;
-  flex-direction: column;
-  background: #cafb3b;
-`;
+  align-items: center;
+  justify-content: center;
 
-const CourseTitle = styled.span`
-  color: var(--xn-hu-5-b-2-ji-36-aba-com-ship-gray, #3f3f46);
-  font-size: 1.5rem;
-  font-weight: 700;
-  line-height: 2.5rem;
-`;
+  width: 100%;
+  height: 14rem;
 
-const CourseContent = styled(CourseTitle)`
-  font-size: 1.3rem;
+  color: #000;
+  font-size: 2rem;
   font-weight: 600;
+  line-height: 2.66667rem;
+  text-align: center;
+
+  background-color: ${({ $team }) => ($team ? '#D9D9D9;' : '#EED7D7;')};
+`;
+
+const DetailsContainer = styled.div`
+  display: flex;
+  padding-top: 8rem;
+  padding-bottom: 17.25rem;
+  justify-content: space-between;
+  gap: 8.5rem;
+`;
+const MountaineImage = styled.div``;
+
+const StyledImage = styled(Image)`
+  width: 26rem;
+  hwight: 26rem;
+`;
+
+const MountainContents = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+`;
+
+const MountainContent = styled.div<{ $outline?: boolean }>`
+  display: flex;
+  flex-direction: ${({ $outline }) => ($outline ? 'column;' : 'row;')};
+  align-items: ${({ $outline }) => ($outline ? 'flex-start;' : 'center;')};
+  gap: ${({ $outline }) => ($outline ? '1.25rem;' : '9rem;')};
+`;
+
+const Title = styled.p`
+  flex: 1;
+  text-wrap: nowrap;
+  color: #000;
+  font-size: 1.6rem;
+  font-weight: 600;
+`;
+
+const Content = styled.span`
+  color: #000;
+  font-size: 1.6rem;
+  font-weight: 400;
+  line-height: 1.8;
+`;
+
+const Container = styled.div`
+  @media (max-width: 768px) {
+    ${StyledImage} {
+      width: 20rem;
+      height: 20rem;
+    }
+
+    ${DetailsContainer} {
+      gap: 3.4rem;
+    }
+  }
+
+  @media (max-width: 480px) {
+    ${DetailsContainer} {
+      display: block;
+    }
+
+    ${StyledImage} {
+      width: 100%;
+      height: auto;
+      margin-bottom: 3.33rem;
+    }
+
+    ${Title} {
+      width: 4.75rem;
+    }
+  }
 `;
 
 interface MountainDetailProps {
   exploreId: number;
   list: mountainDataProps[];
 }
+
 export default function MountainDetail({
   exploreId,
   list,
@@ -76,44 +127,78 @@ export default function MountainDetail({
   return (
     <>
       <Container>
+        <Tab variant="explores" />
+
         <div
           style={{
-            height: '4.6rem',
+            height: '5.7rem',
             backgroundColor: '#ddd',
+            marginTop: '0.4rem',
           }}
         >
-          탭이 들어갈 자리입니다.
+          브레드 크럼블이 들어갈 자리입니다.
         </div>
 
-        <div>
-          <div
-            style={{
-              height: '5.7rem',
-              backgroundColor: '#ddd',
-              marginTop: '0.4rem',
-            }}
-          >
-            브레드 크럼블이 들어갈 자리입니다.
-          </div>
-          <MainTitle>{clickedMountain?.명산_이름}</MainTitle>
+        <MapContainer>
           <KakaoMap
             type="detail"
             filteredItems={clickedMountain ? [clickedMountain] : []}
           />
-        </div>
+        </MapContainer>
         <ActivityTabs>
-          <ActivityTab>등산 모임 n개</ActivityTab>
-          <ActivityTab>등반 후기 n개</ActivityTab>
+          <StyledLink href={`/explores/${exploreId}/teams`}>
+            <ActivityTab $team>
+              등산 모임
+              <br />
+              n개
+            </ActivityTab>
+          </StyledLink>
+          <StyledLink href={`/explores/${exploreId}/reviews`}>
+            <ActivityTab>
+              등반 후기
+              <br />
+              n개
+            </ActivityTab>
+          </StyledLink>
         </ActivityTabs>
-        <CourseInfoArea>
-          <CourseGrid>
-            <CourseInfo>
-              <CourseContent>{clickedMountain?.명산_소재지}</CourseContent>
-              <CourseContent>{clickedMountain?.명산_높이}</CourseContent>
-              <CourseContent>난이도</CourseContent>
-            </CourseInfo>
-          </CourseGrid>
-        </CourseInfoArea>
+        <DetailsContainer>
+          <MountaineImage>
+            <StyledImage
+              width={260}
+              height={260}
+              objectFit="cover"
+              src="/sample.jpg"
+              alt="산 이미지"
+            />
+          </MountaineImage>
+          <MountainContents>
+            <MountainContent $outline>
+              <Title>개요</Title>
+              <Content>
+                지니고 자신에 셈 또 누리다. 엄중의 출발하라고 신은 기업을 괴력에
+                파악하다. 서로 사람의 이러하여 싫은 중간을 것 듯이 보아요.
+                콘크리트가 손도 논의가 면봉을 수 커집니다, 주문하게 없는 경우를
+                않다. 확대되다 지식에 당연히 이어받지요 때문 끊으네. 지 지니고
+                자신에 셈 또 누리다. 엄중의 출발하라고 신은 기업을 괴력에
+                파악하다. 서로 사람의 이러하여 싫은 중간을 것 듯이 보아요.
+                콘크리트가 손도 논의가 면봉을 수 커집니다, 주문하게 없는 경우를
+                않다. 확대되다 지식에 당연히 이어받지요 때문 끊으
+              </Content>
+            </MountainContent>
+            <MountainContent>
+              <Title>산 높이</Title>
+              <Content>{clickedMountain?.명산_높이}m</Content>
+            </MountainContent>
+            <MountainContent>
+              <Title>위치</Title>
+              <Content>{clickedMountain?.명산_소재지}</Content>
+            </MountainContent>
+            <MountainContent>
+              <Title>산 난이도</Title>
+              <Content>중</Content>
+            </MountainContent>
+          </MountainContents>
+        </DetailsContainer>
       </Container>
     </>
   );
