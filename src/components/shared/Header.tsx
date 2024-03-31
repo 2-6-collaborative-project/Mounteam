9747fcc1ebe5403117279865d76951468c7efc36
9ummy9 <ria1005@kakao.com>
2024-03-31 17:48:41 +0900

    Chore: popover 순서 변경

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -67,8 +67,8 @@ export default function Header() {
           content={
             <div>
               <StyledLink href="/teams/create">모임 생성</StyledLink>
-              <StyledLink href="/teams/write">모임후기 등록</StyledLink>
               <StyledLink href="/explores/reviews">등반후기 등록</StyledLink>
+              <StyledLink href="/teams/write">모임후기 등록</StyledLink>
             </div>
           }
         >
