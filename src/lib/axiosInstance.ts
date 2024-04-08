d743b289bc258f5c94c51f98370183896ccad313
ashtonjo <hee576@nate.com>
2024-04-08 11:24:08 +0900

    feat: 피드 상세페이지 타입에 따라 들어가는코드로 변경

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -21,9 +21,9 @@ const axiosAuthApi = (baseURL: string, options?: AxiosRequestConfig) => {
   });
 
   instance.interceptors.request.use((config) => {
-    // const token = `${localStorage.getItem('accessToken')}`; 개발 완료 후 주석 제거
-    const token =
-      'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxOTIyIiwiZXhwIjoxNzEyMjYwMjY0fQ.SsAecOHWTA9vbU8_RVFB42L_y38RcMSRsysRcOPOJHo';
+    const token = `${localStorage.getItem('accessToken')}`;
+    // const token =
+    //   'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxOTIyIiwiZXhwIjoxNzEyMjYyNDUwfQ.MFpmiglj5riPPJtXAFyG3CBnJ4nibpxUnYfHUaXd5jQ';
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
