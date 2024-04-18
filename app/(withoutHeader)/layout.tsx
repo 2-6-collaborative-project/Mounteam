36691f57a45abfffd6cbe5e5e979f3424bdd75a4
9ummy9 <ria1005@kakao.com>
2024-04-18 17:17:22 +0900

    Chore: withoutHeader 메타데이터 수정

--- app/(withoutHeader)/layout.tsx
+++ app/(withoutHeader)/layout.tsx
@@ -17,10 +17,6 @@ export const metadata: Metadata = {
   title: '마운팀',
   description:
     '100대 명산 정보 제공, 등산 코스 추천과 함께 소셜 모임, 뱃지 시스템 등의 경험 공유를 제공하는 등산 커뮤니티 플랫폼',
-  icons: {
-    icon: '/favicon.jpg',
-    //favicon.ico로 변경필요
-  },
 };
 
 export default function RootLayout({
